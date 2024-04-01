b29cad1f43b24e48a3455b515d32ba63bba68cf2
halim <halimml751@gmail.com>
2024-04-02 05:07:16 +0700

    Update cart "add, Get item, remove"

--- entity/cart.go
+++ entity/cart.go
@@ -5,4 +5,34 @@ type CartItem struct {
 	Quantity int
 }
 
+type Cart struct {
+	Item []CartItem
+}
 
+func NewCart() *Cart {
+	return &Cart{}
+}
+
+func (c *Cart) AddItem(name string, quantity int) error {
+	newProd := NewProduct()
+	product, err := newProd.GetProductByName(name)
+	if err != nil {
+		return err
+	}
+	c.Item = append(c.Item, CartItem{product, quantity})
+	return nil
+}
+
+func (c *Cart) GetCart() []CartItem {
+	return c.Item
+}
+
+func (c *Cart) RemoveItem(name string) (string, error) {
+	for i, v := range c.Item {
+		if v.Product.Name == name {
+			c.Item = append(c.Item[:i], c.Item[i+1:]...)
+			return "Success remove from cart", nil
+		}
+	}
+	return "", CustomError{"Product not found"}
+}
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	var cart = entity.NewCart()
 	var customer = entity.NewCustomer()
 	var choose int
 	var acc string
@@ -36,10 +37,9 @@ func main() {
 					fmt.Println(err)
 				} else {
 					fmt.Println(msg)
-					// acc = acc2
-					fmt.Println("Hi, ", acc2)
+					acc = acc2
 				}
-				time.Sleep(3 * time.Second)
+				// time.Sleep(3 * time.Second)
 
 			case 2:
 				fmt.Println("===== Register =====")
@@ -56,7 +56,7 @@ func main() {
 				} else {
 					fmt.Println(msg)
 				}
-				time.Sleep(3 * time.Second)
+				// time.Sleep(3 * time.Second)
 
 			case 3:
 				fmt.Println("Bye")
@@ -64,6 +64,7 @@ func main() {
 
 			default:
 				fmt.Println("Invalid input")
+				time.Sleep(3 * time.Second)
 			}
 		}
 
@@ -72,23 +73,65 @@ func main() {
 			fmt.Println("==============")
 			fmt.Println("1. Add to cart")
 			fmt.Println("2. Show cart")
-			fmt.Println("3. Checkout")
-			fmt.Println("4. Logout")
+			fmt.Println("3. Remove from cart")
+			fmt.Println("4. Checkout")
+			fmt.Println("5. Logout")
 			fmt.Println("==============")
 			fmt.Print("Choose: ")
 			fmt.Scanln(&choose)
 			switch choose {
 			case 1:
-
+				fmt.Println("===== Product =====")
+				for i := range entity.DataProduct {
+					fmt.Println("Neme :", entity.DataProduct[i].Name, "\nPrice :", entity.DataProduct[i].Price, "\nStock :", entity.DataProduct[i].Stock)
+					fmt.Println("==============")
+				}
+				fmt.Print("Enter product name: ")
+				name := ""
+				fmt.Scanln(&name)
+				fmt.Print("Enter quantity: ")
+				quantity := 0
+				fmt.Scanln(&quantity)
+				fmt.Println("")
+				err := cart.AddItem(name, quantity)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Success add to cart")
+				}
+				time.Sleep(3 * time.Second)
 			case 2:
-
+				fmt.Println("===== Show cart =====")
+				for _, v := range cart.GetCart() {
+					fmt.Println("Product :", v.Product.Name, "\nPrice :", v.Product.Price, "\nQuantity :", v.Quantity)
+					fmt.Println("==============")
+				}
+				time.Sleep(5 * time.Second)
 			case 3:
-
+				fmt.Println("===== Remove from cart =====")
+				for i, v := range cart.GetCart() {
+					fmt.Println(i+1, "Product :", v.Product.Name)
+				}
+				fmt.Print("")
+				fmt.Print("Enter product name: ")
+				name := ""
+				fmt.Scanln(&name)
+				msg, err := cart.RemoveItem(name)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(msg)
+				}
+				time.Sleep(3 * time.Second)
 			case 4:
+
+			case 5:
 				fmt.Println("Thanks :)")
 				acc = ""
+				time.Sleep(3 * time.Second)
 			default:
 				fmt.Println("Invalid input")
+				time.Sleep(3 * time.Second)
 			}
 		}
 	}
